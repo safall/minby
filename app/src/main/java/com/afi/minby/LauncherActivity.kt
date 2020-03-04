80a92f08c07feb746cae947d4b2b2422094ce220
Safal <safal.ghimire@tidal.com>
2020-03-04 14:46:56 +0100

    Simplified the launcher activity

--- app/src/main/java/com/afi/minby/LauncherActivity.kt
+++ app/src/main/java/com/afi/minby/LauncherActivity.kt
@@ -3,10 +3,10 @@ package com.afi.minby
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class LauncherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_launcher)
     }
 }
