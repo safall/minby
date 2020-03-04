aa08a477f41e025c9f4a8d9bbcb51a34beba3b21
Safal <safal.ghimire@tidal.com>
2020-03-04 15:11:26 +0100

    Implemented to open login fragment after 2 seconds delay from launcher

--- app/src/main/java/com/afi/minby/auth/launcher/LauncherFragment.kt
+++ app/src/main/java/com/afi/minby/auth/launcher/LauncherFragment.kt
@@ -1,13 +1,15 @@
 package com.afi.minby.auth.launcher
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.NavHostFragment
 import com.afi.minby.R
+import kotlinx.android.synthetic.main.activity_launcher.*
 
 class LauncherFragment : Fragment() {
 
@@ -27,6 +29,8 @@ class LauncherFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(LauncherViewModel::class.java)
-        // TODO: Use the ViewModel
+        Handler().postDelayed({
+            NavHostFragment.findNavController(host_fragment).navigate(R.id.launcherToLoginFragment)
+        }, 2000)
     }
 }
