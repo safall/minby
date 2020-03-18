7132ae9005aff9dfee3e7177bc69f60d48bb7d28
Pardip <pardip235@gmail.com>
2020-03-18 18:20:18 +0100

    Create Message Fragment and message view model

---
+++ app/src/main/java/com/afi/minby/message/MessageFragment.kt
@@ -0,0 +1,34 @@
+package com.afi.minby.message
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.afi.minby.R
+import com.afi.minby.message.viewmodel.MessageViewModel
+
+class MessageFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MessageFragment()
+    }
+
+    private lateinit var viewModel: MessageViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.message_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MessageViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/afi/minby/message/viewmodel/MessageViewModel.kt
@@ -0,0 +1,7 @@
+package com.afi.minby.message.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MessageViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
