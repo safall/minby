567a02a3c42423624afd3cb16cede61b6b79f4b3
Pardip <pardip235@gmail.com>
2020-03-24 13:43:45 +0100

    Created code for speech to text

--- app/src/main/java/com/afi/minby/message/MessageFragment.kt
+++ app/src/main/java/com/afi/minby/message/MessageFragment.kt
@@ -1,18 +1,46 @@
 package com.afi.minby.message
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.afi.minby.R
 import com.afi.minby.message.viewmodel.MessageViewModel
+import kotlinx.android.synthetic.main.message_fragment.*
 
 class MessageFragment : Fragment(R.layout.message_fragment) {
-
     private lateinit var viewModel: MessageViewModel
+    private var VOICE_RECOGNITION_REQUEST_CODE = 200
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MessageViewModel::class.java)
-        // TODO: Use the ViewModel
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        voiceIcon.setOnClickListener { speak() }
+    }
+    private fun speak() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, javaClass.getPackage()!!.name)
+            putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi, Speak something...")
+        }
+        this.startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK) {
+            val text =
+                data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS) as ArrayList<String>
+            messageEditText.text = text[0]
+        }
     }
 }
