ba2754927335f53d838caa06c25c21fbb4700992
Safal <safal.ghimire@tidal.com>
2020-06-06 18:49:37 +0200

    Implemented launcher delay using rx java

--- app/src/main/java/com/afi/minby/auth/launcher/LauncherFragment.kt
+++ app/src/main/java/com/afi/minby/auth/launcher/LauncherFragment.kt
@@ -1,9 +1,8 @@
 package com.afi.minby.auth.launcher
 
 import android.os.Bundle
-import android.os.Handler
-import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.NavHostFragment
 import com.afi.minby.R
@@ -13,21 +12,12 @@ class LauncherFragment : Fragment(R.layout.launcher_fragment) {
 
     private lateinit var viewModel: LauncherViewModel
 
-    private val navigateToLogin = {
-        NavHostFragment.findNavController(host_fragment).navigate(R.id.launcherToLoginFragment)
-    }
-
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(LauncherViewModel::class.java)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        view.handler.postDelayed(navigateToLogin, 2000)
-    }
-
-    override fun onDestroyView() {
-        requireView().handler.removeCallbacks(navigateToLogin)
-        super.onDestroyView()
+        viewModel.loadHomePage.observe(this@LauncherFragment, Observer {
+            NavHostFragment.findNavController(host_fragment).navigate(R.id.launcherToLoginFragment)
+        })
+        viewModel.delay()
     }
 }
--- app/src/main/java/com/afi/minby/auth/launcher/LauncherViewModel.kt
+++ app/src/main/java/com/afi/minby/auth/launcher/LauncherViewModel.kt
@@ -1,7 +1,26 @@
 package com.afi.minby.auth.launcher
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
+import java.util.concurrent.TimeUnit
 
 class LauncherViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    var loadHomePage = MutableLiveData<Boolean>()
+
+    private lateinit var disposable: Disposable
+
+    fun delay() {
+        disposable = Observable.timer(2, TimeUnit.SECONDS)
+            .subscribe {
+                loadHomePage.postValue(true)
+            }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.dispose()
+    }
 }
