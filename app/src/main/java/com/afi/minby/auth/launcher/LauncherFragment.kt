731efddfe61c20f500744f035c0b95f0e7fd4087
Safal <safal.ghimire@tidal.com>
2020-06-07 08:48:53 +0200

    Updated getting viewmodel using new viewmodels API

--- app/src/main/java/com/afi/minby/auth/launcher/LauncherFragment.kt
+++ app/src/main/java/com/afi/minby/auth/launcher/LauncherFragment.kt
@@ -3,19 +3,18 @@ package com.afi.minby.auth.launcher
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import com.afi.minby.R
 import kotlinx.android.synthetic.main.activity_launcher.*
 
 class LauncherFragment : Fragment(R.layout.launcher_fragment) {
 
-    private lateinit var viewModel: LauncherViewModel
-
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(LauncherViewModel::class.java)
-        viewModel.loadHomePage.observe(this@LauncherFragment, Observer {
+        val viewModel = ViewModelProvider(this)
+            .get(LauncherViewModel::class.java)
+        viewModel.loadHomePage.observe(viewLifecycleOwner, Observer {
             NavHostFragment.findNavController(host_fragment).navigate(R.id.launcherToLoginFragment)
         })
         viewModel.delay()
