75d51ee2fbca2448239e650b30f273e20ab574aa
Safal <safal.ghimire@tidal.com>
2020-07-05 11:51:13 +0200

    replaced map fragment with mapview as we dont want the additional childfragment inside location fragment

--- app/src/main/java/com/afi/minby/sendidea/location/EnterLocationFragment.kt
+++ app/src/main/java/com/afi/minby/sendidea/location/EnterLocationFragment.kt
@@ -5,33 +5,53 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import com.afi.minby.R
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_send_idea.*
 import kotlinx.android.synthetic.main.enter_location_fragment.*
 
 class EnterLocationFragment : Fragment(R.layout.enter_location_fragment) {
 
-    private lateinit var googleMap: GoogleMap
-
-    private val callback = OnMapReadyCallback { googleMap ->
-        this.googleMap = googleMap
-        val sydney = LatLng(59.9139, 10.7522)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Oslo"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
+    private var googleMap: GoogleMap? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(callback)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync {
+            googleMap = it
+        }
         nextButton.setOnClickListener {
             NavHostFragment.findNavController(host_fragment)
                 .navigate(R.id.enterLocationFragmentToEnterDetailsFragment)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
     }
 }
