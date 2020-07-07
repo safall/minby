2d9bcef428dae74c3da26cc0caa778e9f031c85a
Safal <safal.ghimire@tidal.com>
2020-07-07 22:03:03 +0200

    Minor simplification for profile fragment

--- app/src/main/java/com/afi/minby/profile/ProfileFragment.kt
+++ app/src/main/java/com/afi/minby/profile/ProfileFragment.kt
@@ -1,7 +1,18 @@
 package com.afi.minby.profile
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.afi.minby.R
+import kotlinx.android.synthetic.main.profile_fragment.*
 
-class ProfileFragment: Fragment(R.layout.profile_fragment) {
+class ProfileFragment : Fragment(R.layout.profile_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        userName.text = "John Doe"
+        userAddress.text = "Hollywood, TX"
+        userEmail.text = "john.doe@johndoe.com"
+        points.text = "84"
+    }
 }
